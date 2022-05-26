e9936966c3f9d07cad5706be35ddafdccc80372d
Felipe Chernicharo <string7dev@gmail.com>
2022-05-26 00:38:08 -0400

    interfaces & improving readability

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,10 +1,27 @@
 import { Client, Motorcycle, Transaction } from '@prisma/client';
 
+interface CompleteTransaction {
+	id: number;
+	client: Client;
+	motorcycles: {
+		id: number;
+		name: string;
+		description: string;
+		year: number;
+		price: number;
+		imgURL: string;
+		quantity: number;
+	}[];
+	total: number;
+	createdAt: string;
+}
+
 export const getTransactionMotos = (
 	clients: Client[],
 	motorcycles: Motorcycle[],
 	transactions: Transaction[]
 ) => {
+	// organize clients and motorcycles in objects
 	let clientsObj: { [x: number]: Client } = {};
 	clients.forEach(client => {
 		if (!clientsObj[client.id]) {
@@ -12,35 +29,47 @@ export const getTransactionMotos = (
 		}
 	});
 
-	let motosObj: { [x: number]: Motorcycle } = {};
+	let motorcyclesObj: { [x: number]: Motorcycle } = {};
 	motorcycles.forEach(moto => {
-		if (!(moto.id in motosObj)) {
-			motosObj[moto.id] = moto;
+		if (!(moto.id in motorcyclesObj)) {
+			motorcyclesObj[moto.id] = moto;
 		}
 	});
 
-	return transactions.map(trans => {
-		const { clientId } = trans;
+	const completeTransactions: CompleteTransaction[] = transactions.map(
+		trans => {
+			const { id, clientId, createdAt } = trans;
 
-		const client = clientsObj[clientId];
+			const client = clientsObj[clientId];
 
-		const parsedMotos = trans.motorcycles.split(',').map(moto => {
-			const [id, qtd] = moto.split(':').map(Number);
-			return { id, qtd };
-		});
+			const decodedMotos = trans.motorcycles.split(',').map(moto => {
+				const [id, quantity] = moto.split(':').map(Number);
+				return { id, quantity };
+			});
 
-		const motorcycles = parsedMotos.map(m => {
-			const motorcycle = motosObj[m.id];
-			return { ...motorcycle, quantity: m.qtd };
-		});
+			const motorcycles = decodedMotos.map(m => {
+				const motorcycle = motorcyclesObj[m.id];
 
-		const total = motorcycles.reduce((acc, moto) => acc + moto.price, 0);
-		return {
-			id: trans.id,
-			createdAt: trans.createdAt,
-			client,
-			motorcycles,
-			total,
-		};
-	});
+				return { ...motorcycle, quantity: m.quantity };
+			});
+
+			const total = motorcycles.reduce(
+				(acc, moto) => acc + moto.price * moto.quantity,
+				0
+			);
+
+			const result = {
+				id,
+				client,
+				motorcycles,
+				total,
+				createdAt,
+				// createdAt: new Date(Date.parse(trans.createdAt)),
+			};
+
+			return result;
+		}
+	);
+
+	return completeTransactions;
 };
